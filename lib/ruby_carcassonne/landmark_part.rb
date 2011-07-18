8cf922e6d961b077925aa9c867e4343c43009a71
Michel Belleville <michel.belleville@gmail.com>
2011-07-18 14:30:23 +0200

    LandmarkPart contacts ok

--- lib/ruby_carcassonne/landmark_part.rb
+++ lib/ruby_carcassonne/landmark_part.rb
@@ -1,4 +1,19 @@
 module RubyCarcassonne::LandmarkPart
+  CONTACTS = [
+    :north_west,
+    :north,
+    :north_east,
+    :east_north,
+    :east,
+    :east_south,
+    :south_east,
+    :south,
+    :south_west,
+    :west_south,
+    :west,
+    :west_north
+  ].reverse.inject({}) { |r, k| r.tap { r[k] = 1 << r.length } }
+
   def initialize sides
     @sides = sides
   end
@@ -6,4 +21,15 @@ module RubyCarcassonne::LandmarkPart
   def sides
     @sides
   end
+
+  def contact?(mask)
+    @sides & mask > 0
+  end
+
+  CONTACTS.each { |contact, mask|
+    method_name = :"#{contact}?"
+    define_method(method_name) {
+      contact? mask
+    }
+  }
 end
--- test/landmark_part_test.rb
+++ test/landmark_part_test.rb
@@ -2,6 +2,8 @@ require_relative 'helper'
 
 class LandmarkPartTest < Test::Unit::TestCase
   context("A nondescript landmark part class") {
+    ###########################################################################
+    # RAW SIDES CONSERVATION
     setup {
       @landmark_part_class = Class.new do
         include RubyCarcassonne::LandmarkPart
@@ -15,6 +17,105 @@ class LandmarkPartTest < Test::Unit::TestCase
         should('show its sides') {
           assert_equal sides, @landmark_part.sides
         }
+      }
+    }
+    ###########################################################################
+    # HUMANLY READABLE CONTACTS
+    context("with no sides") {
+      setup {
+        @landmark_part = @landmark_part_class.new(0b0)
+      }
+      should('have no directions opened') {
+        assert_equal false, @landmark_part.north_west?
+        assert_equal false, @landmark_part.north?
+        assert_equal false, @landmark_part.north_east?
+        assert_equal false, @landmark_part.east_north?
+        assert_equal false, @landmark_part.east?
+        assert_equal false, @landmark_part.east_south?
+        assert_equal false, @landmark_part.south_east?
+        assert_equal false, @landmark_part.south?
+        assert_equal false, @landmark_part.south_west?
+        assert_equal false, @landmark_part.west_south?
+        assert_equal false, @landmark_part.west?
+        assert_equal false, @landmark_part.west_north?
+      }
+    }
+    context("with all sides") {
+      setup {
+        @landmark_part = @landmark_part_class.new(0b111111111111)
+      }
+      should('have all directions opened') {
+        assert_equal true, @landmark_part.north_west?
+        assert_equal true, @landmark_part.north?
+        assert_equal true, @landmark_part.north_east?
+        assert_equal true, @landmark_part.east_north?
+        assert_equal true, @landmark_part.east?
+        assert_equal true, @landmark_part.east_south?
+        assert_equal true, @landmark_part.south_east?
+        assert_equal true, @landmark_part.south?
+        assert_equal true, @landmark_part.south_west?
+        assert_equal true, @landmark_part.west_south?
+        assert_equal true, @landmark_part.west?
+        assert_equal true, @landmark_part.west_north?
+      }
+    }
+    context("with north and east sides only") {
+      setup {
+        @landmark_part = @landmark_part_class.new(0b010010000000)
+      }
+      should('have only the north and east sides opened') {
+        assert_equal false, @landmark_part.north_west?
+        assert_equal true,  @landmark_part.north?
+        assert_equal false, @landmark_part.north_east?
+        assert_equal false, @landmark_part.east_north?
+        assert_equal true,  @landmark_part.east?
+        assert_equal false, @landmark_part.east_south?
+        assert_equal false, @landmark_part.south_east?
+        assert_equal false, @landmark_part.south?
+        assert_equal false, @landmark_part.south_west?
+        assert_equal false, @landmark_part.west_south?
+        assert_equal false, @landmark_part.west?
+        assert_equal false, @landmark_part.west_north?
+      }
+    }
+    context("with the whole south and west only") {
+      setup {
+        @landmark_part = @landmark_part_class.new(0b000000111111)
+      }
+      should('have only the north and east sides opened') {
+        assert_equal false, @landmark_part.north_west?
+        assert_equal false, @landmark_part.north?
+        assert_equal false, @landmark_part.north_east?
+        assert_equal false, @landmark_part.east_north?
+        assert_equal false, @landmark_part.east?
+        assert_equal false, @landmark_part.east_south?
+        assert_equal true,  @landmark_part.south_east?
+        assert_equal true,  @landmark_part.south?
+        assert_equal true,  @landmark_part.south_west?
+        assert_equal true,  @landmark_part.west_south?
+        assert_equal true,  @landmark_part.west?
+        assert_equal true,  @landmark_part.west_north?
+      }
+    }
+    ###########################################################################
+    # RAW CONTACT CHECK
+    context('any landmark placement') {
+      setup {
+        @landmark_part = @landmark_part_class.new(0b010010110101)
+      }
+      should('show all its contacts right') {
+        assert_equal false, @landmark_part.contact?(0b100000000000)
+        assert_equal true,  @landmark_part.contact?(0b010000000000)
+        assert_equal false, @landmark_part.contact?(0b001000000000)
+        assert_equal false, @landmark_part.contact?(0b000100000000)
+        assert_equal true,  @landmark_part.contact?(0b000010000000)
+        assert_equal false, @landmark_part.contact?(0b000001000000)
+        assert_equal true,  @landmark_part.contact?(0b000000100000)
+        assert_equal true,  @landmark_part.contact?(0b000000010000)
+        assert_equal false, @landmark_part.contact?(0b000000001000)
+        assert_equal true,  @landmark_part.contact?(0b000000000100)
+        assert_equal false, @landmark_part.contact?(0b000000000010)
+        assert_equal true,  @landmark_part.contact?(0b000000000001)
       }
     }
   }
