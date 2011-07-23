46a79b014d53a217913da37f75e4026cbf326552
Michel Belleville <michel.belleville@gmail.com>
2011-07-23 15:37:21 +0200

    Cities have constraints for walls positionning

--- lib/ruby_carcassonne/landmark/city.rb
+++ lib/ruby_carcassonne/landmark/city.rb
@@ -1,4 +1,20 @@
 module RubyCarcassonne::Landmark
   class City < Base
+    class SidesCoherenceError < Exception
+    end
+
+    class NoSidesError < Exception
+    end
+
+    def initialize(sides)
+      raise NoSidesError.new if sides == 0
+      s = sides
+      until s == 0 do
+        wall = (s & 0b111)
+        raise SidesCoherenceError.new unless wall == 0b111 || wall == 0b000
+        s = s >> 3
+      end
+      super
+    end
   end
 end
--- test/landmark/city_test.rb
+++ test/landmark/city_test.rb
@@ -13,5 +13,23 @@ class LandmarkCityTest < Test::Unit::TestCase
       }
     }
   }
-  # FIXME : test for invalid cities
+  context("A city with holes in its walls") {
+    should("be invalid") {
+      [ 0b101000000000,
+        0b000001000000,
+        0b000000111110,
+        0b111100111001,
+        0b000000100001
+      ].each { |settings|
+        assert_raise(RubyCarcassonne::Landmark::City::SidesCoherenceError) {
+          RubyCarcassonne::Landmark::City.new(settings) }
+      }
+    }
+  }
+  context("A city with no walls") {
+    should("not exist") {
+      assert_raise(RubyCarcassonne::Landmark::City::NoSidesError) {
+        RubyCarcassonne::Landmark::City.new(0b000000000000) }
+    }
+  }
 end
