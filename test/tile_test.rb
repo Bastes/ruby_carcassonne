91b8cbd9813e8016bfa8162a7f30aeee34711e6f
Michel Belleville <michel.belleville@gmail.com>
2011-07-27 13:34:23 +0200

    Valid tiles are tested

--- test/tile_test.rb
+++ test/tile_test.rb
@@ -28,16 +28,45 @@ class TileTest < Test::Unit::TestCase
           @tile = Tile.new(*landmarks) } }
     }
   }
-  context("A valid tile composed only of only a field and a road") {
-    setup {
-      @landmarks = [
-        Field.new(0b101111101111),
-        Road.new(0b010000010000)]
-      @tile = Tile.new(*@landmarks)
-    }
-    should('show its two landmarks (and only them)') {
-      assert_equal @landmarks.length, @tile.landmarks.length
-      @landmarks.each { |landmark| assert @tile.landmarks.include?(landmark) }
+  [ lambda {
+      [ Field.new(0b101111101111),
+        Road.new(0b010000010000) ] },
+    lambda {
+      [ Monastery.new,
+        Field.new(0b111111111111) ] },
+    lambda {
+      [ City.new(0b111111000000),
+        Field.new(0b000000111111) ] },
+    lambda {
+      [ Field.new(0b100000001111),
+        Field.new(0b001111100000),
+        Road.new(0b010000010000) ] },
+    lambda {
+      [ City.new(0b111000000000),
+        City.new(0b000111000000),
+        Field.new(0b000000111111) ] },
+    lambda {
+      [ City.new(0b111111000000),
+        Field.new(0b000000100001),
+        Field.new(0b000000001100),
+        Road.new(0b000000010010) ] },
+    lambda {
+      [ Field.new(0b100000000001),
+        Field.new(0b001100000000),
+        Field.new(0b000001111100),
+        Road.new(0b010000000000),
+        Road.new(0b000010000000),
+        Road.new(0b000000000010) ] }
+  ].each { |blueprints|
+    context("A tile with landmarks covering all the sides") {
+      setup {
+        @landmarks = blueprints.call
+        @tile = Tile.new(*@landmarks)
+      }
+      should('hold all landmarks properly encased') {
+        assert_equal @landmarks.length, @tile.landmarks.length
+        @landmarks.each { |landmark| assert @tile.landmarks.include?(landmark) }
+      }
     }
   }
 end
