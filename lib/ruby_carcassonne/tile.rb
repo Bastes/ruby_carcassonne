99b112347d30062cd01e809a961e7305524ce617
Michel Belleville <michel.belleville@gmail.com>
2011-07-27 13:11:07 +0200

    Tiles must have all sides covered

--- lib/ruby_carcassonne/tile.rb
+++ lib/ruby_carcassonne/tile.rb
@@ -1,8 +1,12 @@
 module RubyCarcassonne
   class Tile
+    class InvalidTileError < Exception
+    end
+
     attr_reader :landmarks
 
     def initialize(*landmarks)
+      raise InvalidTileError.new unless landmarks.inject(0) { |r, l| r | l.sides } == 0b111111111111
       @landmarks = landmarks
     end
   end
--- test/tile_test.rb
+++ test/tile_test.rb
@@ -1,6 +1,33 @@
 require_relative 'helper'
 
 class TileTest < Test::Unit::TestCase
+  context("Tiles with no landmarks") {
+    should("be invalid") {
+      assert_raise(Tile::InvalidTileError) {
+        @tile = Tile.new }
+    }
+  }
+  context("Tiles with landmarks not covering all sides") {
+    should("be invalid too") {
+      [ [ Monastery.new ],
+        [ Road.new(0b010010) ],
+        [ Field.new(0b001111111100) ],
+        [ City.new(0b000111111111) ],
+        [ Monastery.new,
+          Road.new(0b010000010) ],
+        [ Field.new(0b111101101101),
+          Road.new(0b000010010000) ],
+        [ Field.new(0b000000111111),
+          City.new(0b000111000000) ],
+        [ Monastery.new,
+          Field.new(0b101000101000),
+          City.new(0b000111000111),
+          Road.new(0b010000000000) ]
+      ].each { |landmarks|
+        assert_raise(Tile::InvalidTileError) {
+          @tile = Tile.new(*landmarks) } }
+    }
+  }
   context("A valid tile composed only of only a field and a road") {
     setup {
       @landmarks = [
