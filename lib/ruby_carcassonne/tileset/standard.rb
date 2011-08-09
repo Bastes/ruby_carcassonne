f11e6def9dfbef69e33dfddadadb9f075e8c5cda
Michel Belleville <michel.belleville@gmail.com>
2011-08-09 15:17:56 +0200

    The standard tileset shows 9 fields with L-shaped roads

--- lib/ruby_carcassonne/tileset/standard.rb
+++ lib/ruby_carcassonne/tileset/standard.rb
@@ -1,4 +1,25 @@
 module RubyCarcassonne::Tileset
   class Standard
+    module Tiles
+      class L < RubyCarcassonne::Tile
+        def initialize
+          super(
+            Road.new(0b010010000000),
+            Field.new(0b001100000000),
+            Field.new(0b100001111111))
+        end
+      end
+    end
+
+    attr_reader :tiles
+
+    def initialize
+      @tiles = []
+      9.times { @tiles << Tiles::L.new }
+    end
+
+    def each *args, &block
+      @tiles.each *args, &block
+    end
   end
 end
--- test/tileset/standard_test.rb
+++ test/tileset/standard_test.rb
@@ -5,5 +5,25 @@ class StandardTilesetTest < Test::Unit::TestCase
     should("exist") {
       assert_nothing_raised { RubyCarcassonne::Tileset::Standard }
     }
+    should("exhibit L-shaped road tiles") {
+      template = Tile.new(
+        Road.new(0b010010000000),
+        Field.new(0b001100000000),
+        Field.new(0b100001111111))
+      tile = RubyCarcassonne::Tileset::Standard::Tiles::L.new
+      assert_equal template, tile
+    }
+    context("instance") {
+      setup {
+        @tileset = RubyCarcassonne::Tileset::Standard.new
+      }
+      should("contain 9 different L-shaped road tiles") {
+        template = RubyCarcassonne::Tileset::Standard::Tiles::L.new
+        tiles = @tileset.tiles.select { |tile| tile == template }
+        assert_equal 9, tiles.length
+        (0..7).each { |i| ((i + 1)..8).each { |j|
+          assert !tiles[i].equal?(tiles[j]) } }
+      }
+    }
   }
 end
