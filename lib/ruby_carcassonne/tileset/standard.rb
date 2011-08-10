56f650dc5e179df266c43b5c478efe2b5b8d28fc
Michel Belleville <michel.belleville@gmail.com>
2011-08-10 17:27:39 +0200

    Style improvement on standard tileset

--- lib/ruby_carcassonne/tileset/standard.rb
+++ lib/ruby_carcassonne/tileset/standard.rb
@@ -4,93 +4,93 @@ module RubyCarcassonne::Tileset
       class L < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::Road.new(0b010010000000),
-            RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::Field.new(0b100001111111))
+            RubyCarcassonne::Landmark::Field.new(0b100001111111),
+            RubyCarcassonne::Landmark::Road. new(0b010010000000),
+            RubyCarcassonne::Landmark::Field.new(0b001100000000))
         end
       end
       class I < RubyCarcassonne::Tile
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b100000001111),
-            RubyCarcassonne::Landmark::Road.new(0b010000010000),
+            RubyCarcassonne::Landmark::Road. new(0b010000010000),
             RubyCarcassonne::Landmark::Field.new(0b001111100000))
         end
       end
       class T < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::Field.new(0b100000001111),
-            RubyCarcassonne::Landmark::Road.new(0b010000000000),
-            RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::Road.new(0b000010000000),
+            RubyCarcassonne::Landmark::Field.new(0b111100000001),
+            RubyCarcassonne::Landmark::Road. new(0b000010000000),
             RubyCarcassonne::Landmark::Field.new(0b000001100000),
-            RubyCarcassonne::Landmark::Road.new(0b000000010000))
+            RubyCarcassonne::Landmark::Road. new(0b000000010000),
+            RubyCarcassonne::Landmark::Field.new(0b000000001100),
+            RubyCarcassonne::Landmark::Road. new(0b000000000010))
         end
       end
       class X < RubyCarcassonne::Tile
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b100000000001),
-            RubyCarcassonne::Landmark::Road.new(0b010000000000),
+            RubyCarcassonne::Landmark::Road. new(0b010000000000),
             RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::Road.new(0b000010000000),
+            RubyCarcassonne::Landmark::Road. new(0b000010000000),
             RubyCarcassonne::Landmark::Field.new(0b000001100000),
-            RubyCarcassonne::Landmark::Road.new(0b000000010000),
+            RubyCarcassonne::Landmark::Road. new(0b000000010000),
             RubyCarcassonne::Landmark::Field.new(0b000000001100),
-            RubyCarcassonne::Landmark::Road.new(0b000000000010))
+            RubyCarcassonne::Landmark::Road. new(0b000000000010))
         end
       end
       class C < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111000000000),
-            RubyCarcassonne::Landmark::Field.new(0b000111111111))
+            RubyCarcassonne::Landmark::Field.new(0b111000111111),
+            RubyCarcassonne::Landmark::City. new(0b000111000000))
         end
       end
       class LC < RubyCarcassonne::Tile
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b100001000111),
-            RubyCarcassonne::Landmark::Road.new(0b010010000000),
+            RubyCarcassonne::Landmark::Road. new(0b010010000000),
             RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::City.new(0b000000111000))
+            RubyCarcassonne::Landmark::City. new(0b000000111000))
         end
       end
       class IC < RubyCarcassonne::Tile
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b100000001111),
-            RubyCarcassonne::Landmark::Road.new(0b010000010000),
+            RubyCarcassonne::Landmark::Road. new(0b010000010000),
             RubyCarcassonne::Landmark::Field.new(0b001000100000),
-            RubyCarcassonne::Landmark::City.new(0b000111000000))
+            RubyCarcassonne::Landmark::City. new(0b000111000000))
         end
       end
       class CL < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111000000000),
+            RubyCarcassonne::Landmark::City. new(0b111000000000),
             RubyCarcassonne::Landmark::Field.new(0b000100001111),
-            RubyCarcassonne::Landmark::Road.new(0b000010010000),
+            RubyCarcassonne::Landmark::Road. new(0b000010010000),
             RubyCarcassonne::Landmark::Field.new(0b000001100000))
         end
       end
-      class TC < RubyCarcassonne::Tile
+      class CT < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::Field.new(0b100000001000),
-            RubyCarcassonne::Landmark::Road.new(0b010000000000),
-            RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::Road.new(0b000010000000),
+            RubyCarcassonne::Landmark::City. new(0b111000000000),
+            RubyCarcassonne::Landmark::Field.new(0b000100000001),
+            RubyCarcassonne::Landmark::Road. new(0b000010000000),
             RubyCarcassonne::Landmark::Field.new(0b000001100000),
-            RubyCarcassonne::Landmark::Road.new(0b000000010000),
-            RubyCarcassonne::Landmark::City.new(0b000000000111))
+            RubyCarcassonne::Landmark::Road. new(0b000000010000),
+            RubyCarcassonne::Landmark::Field.new(0b000000001100),
+            RubyCarcassonne::Landmark::Road. new(0b000000000010))
         end
       end
       class D < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111000000),
+            RubyCarcassonne::Landmark::City. new(0b111111000000),
             RubyCarcassonne::Landmark::Field.new(0b000000111111))
         end
       end
@@ -98,54 +98,54 @@ module RubyCarcassonne::Tileset
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b100001000000),
-            RubyCarcassonne::Landmark::Road.new(0b010010000000),
+            RubyCarcassonne::Landmark::Road. new(0b010010000000),
             RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::City.new(0b000000111111))
+            RubyCarcassonne::Landmark::City. new(0b000000111111))
         end
       end
       class H < RubyCarcassonne::Tile
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b111000111000),
-            RubyCarcassonne::Landmark::City.new(0b000111000111))
+            RubyCarcassonne::Landmark::City. new(0b000111000111))
         end
       end
       class A < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111000111),
+            RubyCarcassonne::Landmark::City. new(0b111111000111),
             RubyCarcassonne::Landmark::Field.new(0b000000111000))
         end
       end
       class AR < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111000111),
+            RubyCarcassonne::Landmark::City. new(0b111111000111),
             RubyCarcassonne::Landmark::Field.new(0b000000100000),
-            RubyCarcassonne::Landmark::Road.new(0b000000010000),
+            RubyCarcassonne::Landmark::Road. new(0b000000010000),
             RubyCarcassonne::Landmark::Field.new(0b000000001000))
         end
       end
       class CC < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111000000000),
-            RubyCarcassonne::Landmark::City.new(0b000111000000),
+            RubyCarcassonne::Landmark::City. new(0b111000000000),
+            RubyCarcassonne::Landmark::City. new(0b000111000000),
             RubyCarcassonne::Landmark::Field.new(0b000000111111))
         end
       end
       class CFC < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111000000000),
+            RubyCarcassonne::Landmark::City. new(0b111000000000),
             RubyCarcassonne::Landmark::Field.new(0b000111000111),
-            RubyCarcassonne::Landmark::City.new(0b000000111000))
+            RubyCarcassonne::Landmark::City. new(0b000000111000))
         end
       end
       class Ds < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111000000, true),
+            RubyCarcassonne::Landmark::City. new(0b111111000000, true),
             RubyCarcassonne::Landmark::Field.new(0b000000111111))
         end
       end
@@ -153,38 +153,38 @@ module RubyCarcassonne::Tileset
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b100001000000),
-            RubyCarcassonne::Landmark::Road.new(0b010010000000),
+            RubyCarcassonne::Landmark::Road. new(0b010010000000),
             RubyCarcassonne::Landmark::Field.new(0b001100000000),
-            RubyCarcassonne::Landmark::City.new(0b000000111111, true))
+            RubyCarcassonne::Landmark::City. new(0b000000111111, true))
         end
       end
       class Hs < RubyCarcassonne::Tile
         def self.generate
           self.new(
             RubyCarcassonne::Landmark::Field.new(0b111000111000),
-            RubyCarcassonne::Landmark::City.new(0b000111000111, true))
+            RubyCarcassonne::Landmark::City. new(0b000111000111, true))
         end
       end
       class As < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111000111, true),
+            RubyCarcassonne::Landmark::City. new(0b111111000111, true),
             RubyCarcassonne::Landmark::Field.new(0b000000111000))
         end
       end
       class AsR < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111000111, true),
+            RubyCarcassonne::Landmark::City. new(0b111111000111, true),
             RubyCarcassonne::Landmark::Field.new(0b000000100000),
-            RubyCarcassonne::Landmark::Road.new(0b000000010000),
+            RubyCarcassonne::Landmark::Road. new(0b000000010000),
             RubyCarcassonne::Landmark::Field.new(0b000000001000))
         end
       end
       class Os < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::City.new(0b111111111111, true))
+            RubyCarcassonne::Landmark::City. new(0b111111111111, true))
         end
       end
       class M < RubyCarcassonne::Tile
@@ -197,8 +197,8 @@ module RubyCarcassonne::Tileset
       class MR < RubyCarcassonne::Tile
         def self.generate
           self.new(
-            RubyCarcassonne::Landmark::Field.new(0b101111111111),
-            RubyCarcassonne::Landmark::Road.new(0b010000000000),
+            RubyCarcassonne::Landmark::Field.new(0b111111101111),
+            RubyCarcassonne::Landmark::Road. new(0b000000010000),
             RubyCarcassonne::Landmark::Monastery.new(0b000000000000))
         end
       end
@@ -216,7 +216,7 @@ module RubyCarcassonne::Tileset
         Tiles::LC => 3,
         Tiles::IC => 3,
         Tiles::CL => 3,
-        Tiles::TC => 3,
+        Tiles::CT => 3,
         Tiles::D => 3,
         Tiles::LD => 3,
         Tiles::H => 1,
--- test/tileset/standard_test.rb
+++ test/tileset/standard_test.rb
@@ -24,7 +24,7 @@ class StandardTilesetTest < Test::Unit::TestCase
       "L-shaped road",
       RubyCarcassonne::Tileset::Standard::Tiles::L,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::Road.new(0b010010000000),
+        RubyCarcassonne::Landmark::Road. new(0b010010000000),
         RubyCarcassonne::Landmark::Field.new(0b001100000000),
         RubyCarcassonne::Landmark::Field.new(0b100001111111)))
     should_have_tiles.call(
@@ -33,84 +33,84 @@ class StandardTilesetTest < Test::Unit::TestCase
       RubyCarcassonne::Tileset::Standard::Tiles::I,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b100000001111),
-        RubyCarcassonne::Landmark::Road.new(0b010000010000),
+        RubyCarcassonne::Landmark::Road. new(0b010000010000),
         RubyCarcassonne::Landmark::Field.new(0b001111100000)))
     should_have_tiles.call(
       4,
       "T-shaped road",
       RubyCarcassonne::Tileset::Standard::Tiles::T,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::Field.new(0b100000001111),
-        RubyCarcassonne::Landmark::Road.new(0b010000000000),
-        RubyCarcassonne::Landmark::Field.new(0b001100000000),
-        RubyCarcassonne::Landmark::Road.new(0b000010000000),
+        RubyCarcassonne::Landmark::Field.new(0b111100000001),
+        RubyCarcassonne::Landmark::Road. new(0b000010000000),
         RubyCarcassonne::Landmark::Field.new(0b000001100000),
-        RubyCarcassonne::Landmark::Road.new(0b000000010000)))
+        RubyCarcassonne::Landmark::Road. new(0b000000010000),
+        RubyCarcassonne::Landmark::Field.new(0b000000001100),
+        RubyCarcassonne::Landmark::Road. new(0b000000000010)))
     should_have_tiles.call(
       1,
       "crossroads",
       RubyCarcassonne::Tileset::Standard::Tiles::X,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b100000000001),
-        RubyCarcassonne::Landmark::Road.new(0b010000000000),
+        RubyCarcassonne::Landmark::Road. new(0b010000000000),
         RubyCarcassonne::Landmark::Field.new(0b001100000000),
-        RubyCarcassonne::Landmark::Road.new(0b000010000000),
+        RubyCarcassonne::Landmark::Road. new(0b000010000000),
         RubyCarcassonne::Landmark::Field.new(0b000001100000),
-        RubyCarcassonne::Landmark::Road.new(0b000000010000),
+        RubyCarcassonne::Landmark::Road. new(0b000000010000),
         RubyCarcassonne::Landmark::Field.new(0b000000001100),
-        RubyCarcassonne::Landmark::Road.new(0b000000000010)))
+        RubyCarcassonne::Landmark::Road. new(0b000000000010)))
     should_have_tiles.call(
       5,
       "single city",
       RubyCarcassonne::Tileset::Standard::Tiles::C,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111000000000),
-        RubyCarcassonne::Landmark::Field.new(0b000111111111)))
+        RubyCarcassonne::Landmark::City. new(0b000111000000),
+        RubyCarcassonne::Landmark::Field.new(0b111000111111)))
     should_have_tiles.call(
       3,
       "L-shaped road near a single city",
       RubyCarcassonne::Tileset::Standard::Tiles::LC,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b100001000111),
-        RubyCarcassonne::Landmark::Road.new(0b010010000000),
+        RubyCarcassonne::Landmark::Road. new(0b010010000000),
         RubyCarcassonne::Landmark::Field.new(0b001100000000),
-        RubyCarcassonne::Landmark::City.new(0b000000111000)))
+        RubyCarcassonne::Landmark::City. new(0b000000111000)))
     should_have_tiles.call(
       3,
       "I-shaped road near a single city",
       RubyCarcassonne::Tileset::Standard::Tiles::IC,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b100000001111),
-        RubyCarcassonne::Landmark::Road.new(0b010000010000),
+        RubyCarcassonne::Landmark::Road. new(0b010000010000),
         RubyCarcassonne::Landmark::Field.new(0b001000100000),
-        RubyCarcassonne::Landmark::City.new(0b000111000000)))
+        RubyCarcassonne::Landmark::City. new(0b000111000000)))
     should_have_tiles.call(
       3,
       "single city near an L-shaped road",
       RubyCarcassonne::Tileset::Standard::Tiles::CL,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111000000000),
+        RubyCarcassonne::Landmark::City. new(0b111000000000),
         RubyCarcassonne::Landmark::Field.new(0b000100001111),
-        RubyCarcassonne::Landmark::Road.new(0b000010010000),
+        RubyCarcassonne::Landmark::Road. new(0b000010010000),
         RubyCarcassonne::Landmark::Field.new(0b000001100000)))
     should_have_tiles.call(
       3,
       "T-shaped road near a single city",
-      RubyCarcassonne::Tileset::Standard::Tiles::TC,
+      RubyCarcassonne::Tileset::Standard::Tiles::CT,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::Field.new(0b100000001000),
-        RubyCarcassonne::Landmark::Road.new(0b010000000000),
-        RubyCarcassonne::Landmark::Field.new(0b001100000000),
-        RubyCarcassonne::Landmark::Road.new(0b000010000000),
+        RubyCarcassonne::Landmark::Field.new(0b000100000001),
+        RubyCarcassonne::Landmark::Road. new(0b000010000000),
         RubyCarcassonne::Landmark::Field.new(0b000001100000),
-        RubyCarcassonne::Landmark::Road.new(0b000000010000),
-        RubyCarcassonne::Landmark::City.new(0b000000000111)))
+        RubyCarcassonne::Landmark::Road. new(0b000000010000),
+        RubyCarcassonne::Landmark::Field.new(0b000000001100),
+        RubyCarcassonne::Landmark::Road. new(0b000000000010),
+        RubyCarcassonne::Landmark::City. new(0b111000000000)))
     should_have_tiles.call(
       3,
       "two-sides city",
       RubyCarcassonne::Tileset::Standard::Tiles::D,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111000000),
+        RubyCarcassonne::Landmark::City. new(0b111111000000),
         RubyCarcassonne::Landmark::Field.new(0b000000111111)))
     should_have_tiles.call(
       3,
@@ -118,54 +118,54 @@ class StandardTilesetTest < Test::Unit::TestCase
       RubyCarcassonne::Tileset::Standard::Tiles::LD,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b100001000000),
-        RubyCarcassonne::Landmark::Road.new(0b010010000000),
+        RubyCarcassonne::Landmark::Road. new(0b010010000000),
         RubyCarcassonne::Landmark::Field.new(0b001100000000),
-        RubyCarcassonne::Landmark::City.new(0b000000111111)))
+        RubyCarcassonne::Landmark::City. new(0b000000111111)))
     should_have_tiles.call(
       1,
       "H-shaped city",
       RubyCarcassonne::Tileset::Standard::Tiles::H,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b111000111000),
-        RubyCarcassonne::Landmark::City.new(0b000111000111)))
+        RubyCarcassonne::Landmark::City. new(0b000111000111)))
     should_have_tiles.call(
       3,
       "A-shaped city",
       RubyCarcassonne::Tileset::Standard::Tiles::A,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111000111),
+        RubyCarcassonne::Landmark::City. new(0b111111000111),
         RubyCarcassonne::Landmark::Field.new(0b000000111000)))
     should_have_tiles.call(
       1,
       "A-shaped city with a road",
       RubyCarcassonne::Tileset::Standard::Tiles::AR,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111000111),
+        RubyCarcassonne::Landmark::City. new(0b111111000111),
         RubyCarcassonne::Landmark::Field.new(0b000000100000),
-        RubyCarcassonne::Landmark::Road.new(0b000000010000),
+        RubyCarcassonne::Landmark::Road. new(0b000000010000),
         RubyCarcassonne::Landmark::Field.new(0b000000001000)))
     should_have_tiles.call(
       2,
       "cities in a corner",
       RubyCarcassonne::Tileset::Standard::Tiles::CC,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111000000000),
-        RubyCarcassonne::Landmark::City.new(0b000111000000),
+        RubyCarcassonne::Landmark::City. new(0b111000000000),
+        RubyCarcassonne::Landmark::City. new(0b000111000000),
         RubyCarcassonne::Landmark::Field.new(0b000000111111)))
     should_have_tiles.call(
       3,
       "cities face to face",
       RubyCarcassonne::Tileset::Standard::Tiles::CFC,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111000000000),
+        RubyCarcassonne::Landmark::City. new(0b111000000000),
         RubyCarcassonne::Landmark::Field.new(0b000111000111),
-        RubyCarcassonne::Landmark::City.new(0b000000111000)))
+        RubyCarcassonne::Landmark::City. new(0b000000111000)))
     should_have_tiles.call(
       2,
       "shileded 2-sides city",
       RubyCarcassonne::Tileset::Standard::Tiles::Ds,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111000000, true),
+        RubyCarcassonne::Landmark::City. new(0b111111000000, true),
         RubyCarcassonne::Landmark::Field.new(0b000000111111)))
     should_have_tiles.call(
       2,
@@ -173,38 +173,38 @@ class StandardTilesetTest < Test::Unit::TestCase
       RubyCarcassonne::Tileset::Standard::Tiles::LDs,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b100001000000),
-        RubyCarcassonne::Landmark::Road.new(0b010010000000),
+        RubyCarcassonne::Landmark::Road. new(0b010010000000),
         RubyCarcassonne::Landmark::Field.new(0b001100000000),
-        RubyCarcassonne::Landmark::City.new(0b000000111111, true)))
+        RubyCarcassonne::Landmark::City. new(0b000000111111, true)))
     should_have_tiles.call(
       2,
       "shielded H-shaped city",
       RubyCarcassonne::Tileset::Standard::Tiles::Hs,
       RubyCarcassonne::Tile.new(
         RubyCarcassonne::Landmark::Field.new(0b111000111000),
-        RubyCarcassonne::Landmark::City.new(0b000111000111, true)))
+        RubyCarcassonne::Landmark::City. new(0b000111000111, true)))
     should_have_tiles.call(
       1,
       "shielded A-shaped city",
       RubyCarcassonne::Tileset::Standard::Tiles::As,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111000111, true),
+        RubyCarcassonne::Landmark::City. new(0b111111000111, true),
         RubyCarcassonne::Landmark::Field.new(0b000000111000)))
     should_have_tiles.call(
       2,
       "shielded A-shaped city with a road",
       RubyCarcassonne::Tileset::Standard::Tiles::AsR,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111000111, true),
+        RubyCarcassonne::Landmark::City. new(0b111111000111, true),
         RubyCarcassonne::Landmark::Field.new(0b000000100000),
-        RubyCarcassonne::Landmark::Road.new(0b000000010000),
+        RubyCarcassonne::Landmark::Road. new(0b000000010000),
         RubyCarcassonne::Landmark::Field.new(0b000000001000)))
     should_have_tiles.call(
       1,
       "shielded complete city",
       RubyCarcassonne::Tileset::Standard::Tiles::Os,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::City.new(0b111111111111, true)))
+        RubyCarcassonne::Landmark::City. new(0b111111111111, true)))
     should_have_tiles.call(
       4,
       "simple monastery",
@@ -217,8 +217,8 @@ class StandardTilesetTest < Test::Unit::TestCase
       "monastery with a road",
       RubyCarcassonne::Tileset::Standard::Tiles::MR,
       RubyCarcassonne::Tile.new(
-        RubyCarcassonne::Landmark::Field.new(0b101111111111),
-        RubyCarcassonne::Landmark::Road.new(0b010000000000),
+        RubyCarcassonne::Landmark::Field.new(0b111111101111),
+        RubyCarcassonne::Landmark::Road. new(0b000000010000),
         RubyCarcassonne::Landmark::Monastery.new(0b000000000000)))
   }
 end
